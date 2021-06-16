e492650896cbdb68c741db38ab46cc205b1497c7
Artur Szott <artur.szott@gmail.com>
2021-06-16 16:32:39 +0200

    Handle loops and dead ends within graph

--- src/graphs/isPathBetweenNodes.test.ts
+++ src/graphs/isPathBetweenNodes.test.ts
@@ -32,9 +32,61 @@ describe('isPathBetweenNodes', () => {
 				nodes
 			);
 
-			new Graph({ nodes, connections, isDirected: false });
+			new Graph({ nodes, connections, isDirected: true });
 
-			expect(isPathBetweenNodes(nodes[0], nodes[3]));
+			expect(isPathBetweenNodes(nodes[0], nodes[3])).toBe(true);
+		});
+
+		test('with one dead end branch', () => {
+			const nodes = Array.from({ length: 4 }, (_, i) => new Node(i));
+			const connections = createConnectionsByIndex(
+				[
+					[0, 1],
+					[0, 2],
+					[2, 3]
+				],
+				nodes
+			);
+
+			new Graph({ nodes, connections, isDirected: true });
+
+			expect(isPathBetweenNodes(nodes[0], nodes[3])).toBe(true);
+		});
+
+		test('with one loop', () => {
+			const nodes = Array.from({ length: 8 }, (_, i) => new Node(i));
+			const connections = createConnectionsByIndex(
+				[
+					[0, 1],
+					[1, 0],
+					[1, 2],
+					[2, 3],
+					[3, 4],
+					[4, 5]
+				],
+				nodes
+			);
+
+			new Graph({ nodes, connections, isDirected: true });
+
+			expect(isPathBetweenNodes(nodes[0], nodes[5])).toBe(true);
+		});
+	});
+
+	describe('should tell if there is NO path between nodes', () => {
+		test('with one loop', () => {
+			const nodes = Array.from({ length: 8 }, (_, i) => new Node(i));
+			const connections = createConnectionsByIndex(
+				[
+					[0, 1],
+					[1, 0]
+				],
+				nodes
+			);
+
+			new Graph({ nodes, connections, isDirected: true });
+
+			expect(isPathBetweenNodes(nodes[0], nodes[5])).toBe(false);
 		});
 	});
 });
--- src/graphs/isPathBetweenNodes.ts
+++ src/graphs/isPathBetweenNodes.ts
@@ -4,17 +4,23 @@ import { Node } from './utils';
 export default function isPathBetweenNodes(nodeA: Node, nodeB: Node): boolean {
 	const queue = new Queue<Node>();
 
+	let visitedNodes = 0;
+
 	queue.push(nodeA);
 
 	while (!queue.isEmpty()) {
 		const currentNode = queue.pop();
+		currentNode.data.visited = true;
+		visitedNodes++;
 
 		if (currentNode.data === nodeB) {
 			return true;
 		}
 
 		currentNode.data.adjascent.forEach((adjascentNode: Node) => {
-			queue.push(adjascentNode);
+			if (!adjascentNode.visited) {
+				queue.push(adjascentNode);
+			}
 		});
 	}
 
